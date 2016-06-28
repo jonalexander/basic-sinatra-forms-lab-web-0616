e58539e167e04aa711eda2659fe539410f2aff71
Jonathan Alexander <jonathandalexander@gmail.com>
2016-06-28 12:16:43 -0400

    Done.

--- app.rb
+++ app.rb
@@ -7,15 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do 
-    @team_array = [
-      @name = params["name"],
-      @coach = params["coach"],
-      @pg = params["pg"],
-      @sg = params["sg"],
-      @sf = params["sf"],
-      @pf = params["pf"],
+      @name = params["name"]
+      @coach = params["coach"]
+      @pg = params["pg"]
+      @sg = params["sg"]
+      @sf = params["sf"]
+      @pf = params["pf"]
       @c = params["c"]
-    ]
     erb :team
   end
 
